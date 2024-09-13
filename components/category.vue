ef9e57b1475870a99df33593e35dc10dee46b0c5
shu-0-lycorish <shu.kwn0257b@gmail.com>
2024-09-14 01:03:25 +0900

    install Nuxt Image

--- components/category.vue
+++ components/category.vue
@@ -2,11 +2,14 @@
     <div>
         <h1 class="text-black text-3xl font-bold">Category</h1>
         <div v-for="singleData in data" :key="singleData.id">
-                <div>
-                    <h3>{{ singleData.name }}</h3>
-                    <p>{{ singleData.price }}</p>
-                </div>
+            <div>
+                <nuxt-img :src="singleData.image" alt="product-image" format="webp" />
             </div>
+            <div>
+                <h3>{{ singleData.name }}</h3>
+                <p>{{ singleData.price }}</p>
+            </div>
+        </div>
     </div>
 </template>
 
