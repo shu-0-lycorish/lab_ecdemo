d59311d410d204643279ee87eb71c50159d20230
shu-0-lycorish <shu.kwn0257b@gmail.com>
2024-09-14 00:20:30 +0900

    add category component file

---
+++ components/category.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1 class="text-black text-3xl font-bold">Category</h1>
+        <div v-for="singleData in data" :key="singleData.id">
+                <div>
+                    <h3>{{ singleData.name }}</h3>
+                    <p>{{ singleData.price }}</p>
+                </div>
+            </div>
+    </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData("productQuery", () =>
+    queryContent("/product").sort({productNum: -1}).find()
+)
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <h1 class="text-black text-3xl font-bold">TopPage</h1>
+    <Category />
 </template>
 
 <script setup>
