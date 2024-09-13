1877d0e6843a6e947fde21ad0322c5d42b4536b8
shu-0-lycorish <shu.kwn0257b@gmail.com>
2024-09-14 01:25:55 +0900

    add top_slide file

---
+++ components/top_slide.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div>
+            <nuxt-img src="/images/0101.jpg" alt="product-image" format="webp" />
+        </div>
+        <div>
+            <h2>Product Title</h2>
+            <p>商品説明の概要が入ります．</p>
+            <h4>¥3,500 (税込)</h4>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <h1 class="text-black text-3xl font-bold">TopPage</h1>
+    <TopSlide />
     <Category />
 </template>
 
