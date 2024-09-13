1c23c3e5c449ac102f9d59628cd62edf0891377b
shu-0-lycorish <shu.kwn0257b@gmail.com>
2024-09-14 01:18:56 +0900

    Completed category.vue

--- components/category.vue
+++ components/category.vue
@@ -1,20 +1,19 @@
 <template>
     <div>
         <h1 class="text-black text-3xl font-bold">Category</h1>
-        <div v-for="singleData in data" :key="singleData.id">
+        <div>
             <div>
-                <nuxt-img :src="singleData.image" alt="product-image" format="webp" />
+                <nuxt-img src="/images/0101.jpg" alt="product-image" format="webp" />
+                <p>Light</p>
             </div>
             <div>
-                <h3>{{ singleData.name }}</h3>
-                <p>{{ singleData.price }}</p>
+                <nuxt-img src="/images/0201.jpg" alt="product-image" format="webp" />
+                <p>Clock</p>
+            </div>
+            <div>
+                <nuxt-img src="/images/0301.jpg" alt="product-image" format="webp" />
+                <p>Sofa</p>
             </div>
         </div>
     </div>
 </template>
-
-<script setup>
-const { data } = await useAsyncData("productQuery", () =>
-    queryContent("/product").sort({productNum: -1}).find()
-)
-</script>
