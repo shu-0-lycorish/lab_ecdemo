ac97f828fd53edd03570f13158a8caf942912125
shu-0-lycorish <shu.kwn0257b@gmail.com>
2024-09-19 02:57:03 +0900

    setting DaisyUI

--- components/top_slide.vue
+++ components/top_slide.vue
@@ -4,7 +4,7 @@
             <nuxt-img src="/images/0101.jpg" alt="product-image" format="webp" />
         </div>
         <div>
-            <h2>Product Title</h2>
+            <h2 class="btn">Product Title</h2>
             <p>商品説明の概要が入ります．</p>
             <h4>¥3,500 (税込)</h4>
         </div>
