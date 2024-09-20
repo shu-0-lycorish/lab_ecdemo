fccb696ca499563136c69893a95d68c0cfbea37c
shu-0-lycorish <shu.kwn0257b@gmail.com>
2024-09-20 22:10:07 +0900

    add CSS for footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,22 +1,29 @@
 <template>
     <footer>
-        <div>
-            <div>
-                <h1>ThesisMart</h1>
+        <div class="footer bg-base-200 text-base-content px-[8%] py-20 space-x-12">
+            <aside class="space-y-4 w-[56vw]">
+                <h1 class="text-4xl">ThesisMart</h1>
                 <p>Demo EC Site for Thesis</p>
-            </div>
-            <div>
-                <ul>
-                    <li>PRODUCTS</li>
-                    <li>NEWS</li>
-                    <li>MAGAZINE</li>
-                    <li>GUIDE</li>
-                    <li>CONTACT</li>
-                    <li>COMPANY</li>
-                    <li>PRIVACY POLICY</li>
-                </ul>
-            </div>
-            <p>©ThesisMart created by Shuhei Kawano</p>
+            </aside>
+            <nav>
+                <a class="link link-hover text-xl">PRODUCTS</a>
+                <a class="link link-hover text-xl">NEWS</a>
+                <a class="link link-hover text-xl">MAGAZINE</a>
+                <a class="link link-hover text-xl">GUIDE</a>
+            </nav>
+            <nav>
+                <a class="link link-hover text-xl">CONTACT</a>
+                <a class="link link-hover text-xl">COMPANY</a>
+                <a class="link link-hover text-xl">PRIVASY POLISY</a>
+            </nav>
         </div>
+    </footer>
+
+    <footer class="footer bg-base-200 text-base-content border-base-300 border-t px-[8%] py-10">
+        <aside class="grid-flow-col items-center">
+            <div>
+                <p>©ThesisMart created by Shuhei Kawano</p>
+            </div>
+        </aside>
     </footer>
 </template>
