018a6b1be71a334f49cec715df8a92e3cd0fafbf
shu-0-lycorish <shu.kwn0257b@gmail.com>
2024-09-26 01:43:43 +0900

    add CSS for product detail

--- pages/product/[itemId].vue
+++ pages/product/[itemId].vue
@@ -1,15 +1,27 @@
 <template>
-    <div>
-        <div>
+    <div class="grid grid-cols-12 gap-8 my-16 relative mx-[8%] min-h-[52vh] place-content-start">
+        <div class="col-span-7">
             <nuxt-img :src="data.image" alt="blog-image" format="webp" />
         </div>
-        <div>  
-            <div>               
-                <h1>{{ data.name }}</h1>
-                <p>{{ data.price }}</p> 
-                <ContentDoc />
-            </div> 
+        <div class="col-span-5 content-center space-y-8">
+            <div class="space-y-2">
+                <h2 class="text-3xl text-black">{{ data.name }}</h2>
+                <p class="text-lg text black">{{ data.summary }}</p>
+            </div>
+            <h4 class="text-xl text-black">¥ {{ data.price }}  (税込)</h4>
+            <div>
+                <NuxtLink to="/">
+                    <button class="btn btn-primary text-lg">カートに入れる</button>
+                </NuxtLink>
+            </div>
         </div>
+        <div class="col-span-12 my-16 space-y-8">
+            <h2 class="text-3xl">商品説明</h2>
+            <div>
+                <ContentDoc />
+            </div>
+        </div>
+
     </div>
 </template>
 
