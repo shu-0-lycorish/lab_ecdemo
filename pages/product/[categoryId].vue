acac475de0872f87e6cc1925d49b621f80c59845
shu-0-lycorish <shu.kwn0257b@gmail.com>
2024-09-25 23:24:42 +0900

    create product list page

---
+++ pages/product/[categoryId].vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1 class="text-black text-3xl font-bold">Category</h1>
+        <div v-for="singleData in data" :key="singleData.id">
+            <div>
+                <nuxt-img :src="singleData.image" alt="product-image" format="webp" />
+            </div>
+            <div>
+                <h3>{{ singleData.name }}</h3>
+                <p>{{ singleData.price }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const path = useRoute().path
+var category = path.replace('/product/', '')
+
+const { data } = await useAsyncData("productQuery2", () =>
+    queryContent("/product").where({categoryId: Number(category)}).find()
+)
+
+console.log(category)
+</script>
---
+++ pages/product/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1 class="text-black text-3xl font-bold">Category</h1>
+        <div v-for="singleData in data" :key="singleData.id">
+            <div>
+                <nuxt-img :src="singleData.image" alt="product-image" format="webp" />
+            </div>
+            <div>
+                <h3>{{ singleData.name }}</h3>
+                <p>{{ singleData.price }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData("productQuery3", () =>
+    queryContent("/product").where({categoryId: 2}).sort({productId: -1}).find()
+)
+</script>
