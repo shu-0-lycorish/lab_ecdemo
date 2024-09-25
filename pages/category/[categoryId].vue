4f64d461407ffa04fbd954eb9e31aa4c1f682928
shu-0-lycorish <shu.kwn0257b@gmail.com>
2024-09-26 00:30:27 +0900

    add CSS for product list

--- pages/category/[categoryId].vue
+++ pages/category/[categoryId].vue
@@ -1,26 +1,36 @@
 <template>
-    <div>
-        <h1 class="text-black text-3xl font-bold">Category</h1>
-        <div v-for="singleData in data" :key="singleData.id">
-            <NuxtLink :to="singleData._path">
-                <div>
-                    <nuxt-img :src="singleData.image" alt="product-image" format="webp" />
-                </div>
-                <div>
-                    <h3>{{ singleData.name }}</h3>
-                    <p>{{ singleData.price }}</p>
-                </div>
-            </NuxtLink>
+    <div class="grid grid-cols-12 gap-8 my-16 relative mx-[8%] min-h-[52vh] place-content-start">
+        <div class="col-span-12">
+            <div class="text-black text-3xl font-bold">
+                <h2 v-if="category === 1">Light</h2>
+                <h2 v-if="category === 2">Clock</h2>
+                <h2 v-if="category === 3">Sofa</h2>
+            </div>
+        </div>
+        <div class="grid grid-cols-subgrid col-span-12">
+            <div v-for="singleData in data" :key="singleData.id" class="col-span-4 space-y-4">
+                <NuxtLink :to="singleData._path">
+                    <div class="card bg-base-100 shadow-xl">
+                        <figure>
+                            <nuxt-img :src="singleData.image" alt="product-image" format="webp" />
+                        </figure>
+                        <div class="card-body">
+                            <h3 class="text-2xl text-black">{{ singleData.name }}</h3>
+                            <p class="text-lg text-black">{{ singleData.price }}</p>
+                        </div>
+                    </div>
+                </NuxtLink>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 const path = useRoute().path
-var category = path.replace('/category/', '')
+var category = Number(path.replace('/category/', ''))
 
 const { data } = await useAsyncData("productQuery2", () =>
-    queryContent("/product").where({categoryId: Number(category)}).find()
+    queryContent("/product").where({ categoryId: category }).find()
 )
 
 console.log(category)
