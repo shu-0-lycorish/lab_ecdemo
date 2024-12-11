62c4217cb8aae1aeb15fe7a42948132ada2d01d3
shu-0-lycorish <shu.kwn0257b@gmail.com>
2024-12-12 00:44:46 +0900

    input in topslide

--- components/top_slide.vue
+++ components/top_slide.vue
@@ -5,13 +5,13 @@
                 <div class="card bg-base-100 shadow-xl">
                     <nuxt-img src="/images/0303.jpg" alt="product-image" format="webp" />
                 </div>
-                <div class="absolute bottom-6 left-6 w-4/12">
-                    <div class="flex flex-col space-y-4 px-6 py-4 bg-white">
-                        <div>
-                            <h2 class="text-4xl text-black font-light">ProductName0303</h2>
-                            <p class="text-black">これは要約文です．これは要約文です．これは要約文です．</p>
+                <div class="absolute bottom-6 left-6 w-5/12">
+                    <div class="flex flex-col space-y-4 px-6 py-6 bg-white">
+                        <div class="space-y-2">
+                            <h2 class="text-4xl text-black font-light">ヴィンテージグレイウィングバックソファ</h2>
+                            <p class="text-black">優雅な背もたれデザインが特徴のクラシックなグレーのソファは、上品で落ち着いた雰囲気を提供します。</p>
                         </div>
-                        <h4 class="text-2xl text-black">¥10,000 (税込)</h4>
+                        <h4 class="text-2xl text-black">¥68000 (税込)</h4>
                     </div>
                 </div>
             </NuxtLink>
